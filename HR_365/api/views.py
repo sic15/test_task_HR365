f0492b0266bb8e1582c637e5d266c1605e06e670
Наталья Арлазарова <sic15@yandex.ru>
2023-09-15 00:59:02 +0300

    update views.py

--- HR_365/api/views.py
+++ HR_365/api/views.py
@@ -1,7 +1,5 @@
 from rest_framework.decorators import api_view
-from django.http import HttpResponse
 from rest_framework.response import Response
-from rest_framework import status
 import requests
 
 
